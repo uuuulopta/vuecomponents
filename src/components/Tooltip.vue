c1abb69d37a98b5302d3f9752bcb8adfa6cb5b15
Ivan Dvornicki <uuuulopta@gmail.com>
2023-01-02 19:12:54 +0100

    Removed opacity mouseover transition for now due to it causing issues

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,6 +1,6 @@
 <template>
     <div :id="id" data-show="false" role="tooltip" :style="style" @mouseover="hover"
-    class="absolute max-w-sm break-words z-10 py-1 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 dark:bg-gray-700">
+    class="absolute max-w-sm break-words mb-2 z-10 py-1 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 dark:bg-gray-700">
     <div ref="slot"><slot></slot></div>
 </div>
 
@@ -45,9 +45,10 @@ target.addEventListener('mouseover', () => {
 
 })
 target.addEventListener('mouseout', () => {
-    tooltip.style = "opacity:0;"
-    setTimeout(() => {tooltip.style="vibility:hidden;"},300)
-    popper.update();
+    tooltip.style = "opacity:0;visibility:hidden;"
+
+    
+
 })
 
 
