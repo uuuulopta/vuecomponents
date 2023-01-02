22b774e8c2856f34da8eba6bdac97c8da995fa9a
Ivan Dvornicki <uuuulopta@gmail.com>
2023-01-02 19:04:45 +0100

    Fixed hover not appearing when tooltips are overlapping the target element

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,6 +1,6 @@
 <template>
     <div :id="id" data-show="false" role="tooltip" :style="style" @mouseover="hover"
-    class="absolute z-10 py-1 px-3 text-xs font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 dark:bg-gray-700">
+    class="absolute max-w-sm break-words z-10 py-1 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 dark:bg-gray-700">
     <div ref="slot"><slot></slot></div>
 </div>
 
@@ -12,7 +12,7 @@
 </style>
 
 <script setup>
-import {onMounted, ref,} from 'vue';
+import {onMounted, onUpdated, ref,} from 'vue';
 import { createPopper } from '@popperjs/core';
 const props = defineProps({
     id: {
@@ -27,18 +27,26 @@ let slot = ref();
 onMounted( () => {
 const tooltip = document.getElementById(props.id);
 const target = document.getElementById(props.explains);
+tooltip.style = "visibility:hidden;"
+
 const popper = createPopper(target,tooltip, {
     placement: 'top',
 
 });
 
+onUpdated( () => {
+    popper.update();
+})
+
 
 target.addEventListener('mouseover', () => {
-    tooltip.style = "opacity:1;"
+    tooltip.style = "opacity:1;visibility:visible;"
     popper.update();
+
 })
 target.addEventListener('mouseout', () => {
     tooltip.style = "opacity:0;"
+    setTimeout(() => {tooltip.style="vibility:hidden;"},300)
     popper.update();
 })
 
